5a712a8ef9cb29b4d5b48aa89d9f2c32e935a4bb
MaheshReddy-05 <nagamaheshreddy3@gmail.com>
2022-11-08 15:58:04 +0530

    SQL Constrains

--- basicMysql.sql
+++ basicMysql.sql
@@ -20,4 +20,70 @@ ADD column Location varchar(40);
 ALTER TABLE student 
 MODIFY COLUMN RollNo varchar(20);
 
+-- Alter Table Drop column 
+ALTER TABLE student
+DROP COLUMN Location;
 
+-- Drop Table 
+DROP TABLE student;
+
+-- Drop Database
+DROP DATABASE university;
+
+-- SQL Constraints Examples
+
+-- NOT NULL Constrain
+CREATE TABLE IF NOT EXISTS student(
+    StudID INT NOT NULL,
+    Name VARCHAR(200) NOT NULL,
+    Age INT,
+    City VARCHAR(30)
+);
+
+-- CHECK Constrain
+CREATE TABLE IF NOT EXISTS student(
+    StudID INT NOT NULL,
+    Name VARCHAR(200) NOT NULL,
+    Age INT,
+    City VARCHAR(30),
+    CHECK (Age>=18) 
+);
+
+--  DEFAULT Constrain
+CREATE TABLE IF NOT EXISTS student(
+    StudID INT NOT NULL,
+    Name VARCHAR(200) NOT NULL,
+    Age INT,
+    City VARCHAR(30) DEFAULT 'NRT', 
+);
+
+-- UNIQUE Cpnstrain
+CREATE TABLE IF NOT EXISTS student(
+    StudID INT NOT NULL UNIQUE,
+    Name VARCHAR(200) NOT NULL,
+    Age INT,
+    City VARCHAR(30),
+    CHECK (Age>=18) 
+);
+
+-- PRIMARY KEY Constrain
+CREATE TABLE IF NOT EXISTS student(
+    StudID INT NOT NULL UNIQUE,
+    Name VARCHAR(200) NOT NULL,
+    Age INT,
+    City VARCHAR(30),
+    CHECK (Age>=18),
+    PRIMARY KEY (StudID)
+);
+
+-- FOREIGN KEY Constrain
+CREATE TABLE IF NOT EXISTS student(
+    StudID INT NOT NULL,
+    Name VARCHAR(200) NOT NULL,
+    Age INT,
+    City VARCHAR(30),
+    PRIMARY KEY (StudID),
+    FOREIGN KEY (Age) REFERENCES agetable(Age)
+);
+
+-- Time 33 min Link https://www.youtube.com/watch?v=5YCeDPITsZA&list=PLrk5tgtnMN6RP0PMnyBVI15N_phCQevZM&index=13
